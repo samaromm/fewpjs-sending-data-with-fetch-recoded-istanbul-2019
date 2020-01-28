69bca2ff9a44848ef03823e6cd44b31cd8530ba1
Samar Omar <samaromm98@gmail.com>
2020-01-28 23:50:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(uName, uEmail){
+  let formData = {
+  name: uName,
+  email: uEmail
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.append(object.id);
+  })
+  .catch(function(error) {
+    document.body.append(error.message)
+  });
+}
