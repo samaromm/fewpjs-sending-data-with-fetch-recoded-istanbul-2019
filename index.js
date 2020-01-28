e4f6cbf07e9f8d38588a8c2398441ef660c77a7a
Samar Omar <samaromm98@gmail.com>
2020-01-28 23:38:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // Add your code here
 
 function submitData(uName, uEmail){
-  
+  let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+// method: "POST" is missing from the object below
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
 }
